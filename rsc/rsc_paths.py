abd684772e54066c2356309734d9458799b6a159
Evening <johnchangqi@hotmail.com>
2020-08-18 16:42:31 +0800

    Upload GLCM Test

--- rsc/rsc_paths.py
+++ rsc/rsc_paths.py
@@ -3,13 +3,18 @@ import os
 _RSC = os.path.dirname(os.path.realpath(__file__)) + "/../rsc/"
 
 _RAW = _RSC + "raws/"
-_SAMPLE = _RSC + "samples/"
+_EXTRACT = _RSC + "extracts/"
 
 _RAW_VIDS = _RAW + "videos/"
-_RAW_IMGS = _RAW + "images/"
+_RAW_IMGS = _RAW + "imgs/"
 
-SAMPLE_IMGS = _SAMPLE + "images/"
-SAMPLE_VIDS = _SAMPLE + "videos/"
+EXTRACT_IMGS = _EXTRACT + "imgs/"
+EXTRACT_VIDS = _EXTRACT + "videos/"
+
+# Quick way to get a sample img
+SAMPLE_CHESTNUT_0S_IMG   = EXTRACT_IMGS + "chestnut_0/frame0ms.jpg"
+SAMPLE_CHESTNUT_50S_IMG  = EXTRACT_IMGS + "chestnut_0/frame50000ms.jpg"
+SAMPLE_CHESTNUT_100S_IMG = EXTRACT_IMGS + "chestnut_0/frame100000ms.jpg"
 
 VID_CHESTNUT_0 = _RAW_VIDS + "chestnut_0.mp4"
 
