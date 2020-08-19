30d441e5175dc87a2c41fb230f2b2f5c085fe6ba
Evening <johnchangqi@hotmail.com>
2020-08-19 15:20:25 +0800

    Add Comments on GLCM Test

--- tests/base/D2/GLCM/test.py
+++ tests/base/D2/GLCM/test.py
@@ -2,20 +2,28 @@ import unittest
 
 from rsc.samples.frames import chestnut_0
 from FRModel.base.consts import CONSTS
-from FRModel.base.D2.frame2D import Frame2D
 
 
 class GLCMTest(unittest.TestCase):
 
     def test(self):
 
+        # Load in sample
         frame = chestnut_0(0)
+
+        # Create 2-deep nested list of 100x100 frames
         frames = frame.split_xy(100)
 
+        # Loop through the list
         for xframes in frames:
             for frame in xframes:
+                # Grab red channel
                 frame_red = frame.channel(CONSTS.CHANNEL.RED)
+
+                # Create the pseudo-matrix with .glcm
                 glcm = frame_red.glcm(by=1, axis=CONSTS.AXIS.Y)
+
+                # Extract statistics of each window
                 glcm.contrast()
                 glcm.correlation()
                 glcm.entropy()
