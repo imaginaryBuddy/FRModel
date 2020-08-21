1c2e3245ef709325cc6b404f5cd74ff2bc2f6514
Evening <johnchangqi@hotmail.com>
2020-08-21 17:13:40 +0800

    Add Shape validation on new dimensional order

--- tests/base/D2/Frame/test.py
+++ tests/base/D2/Frame/test.py
@@ -3,28 +3,38 @@ import unittest
 import numpy as np
 
 from frmodel.base.D2.frame2D import Frame2D
+from frmodel.base.D2.channel2D import Channel2D
 from frmodel.base.consts import CONSTS
 from rsc.samples.frames import chestnut_0
 
 
 class FrameTest(unittest.TestCase):
 
-    def test(self):
+    def __init__(self):
+        super().__init__()
+        self.frame = chestnut_0(0)
 
-        # Grab Sample
-        frame = chestnut_0(0)
-
+    def test_split_xy(self):
         # Split by both X and Y
-        frames = frame.split_xy(by=100, method=Frame2D.SplitMethod.DROP)
+        frames = self.frame.split_xy(by=100, method=Frame2D.SplitMethod.DROP)
 
+        self.assertEqual(frames[0][0].shape, (100, 100))
+
+    def test_split(self):
+        # Split by Y Axis only, horizontal slices
+        frames = self.frame.split(by=100, method=Frame2D.SplitMethod.DROP, axis=CONSTS.AXIS.Y)
+
+        self.assertEqual(frames[0].shape[0], 100)
+
+    def test_flatten(self):
+        self.assertEqual(self.frame.data_flatten().shape, (*self.frame.shape, 3))
+
+    def test_channel(self):
         # Grab the red channel
-        red_channel = frames[0][0].channel(CONSTS.CHANNEL.RED)
+        red = self.frame.channel(CONSTS.CHANNEL.RED)
 
-        # Get underlying data, numpy ndarray
-        ar = red_channel.data
+        self.assertIsInstance(red, Channel2D)
 
-        # Assert above is true
-        self.assertIsInstance(ar, np.ndarray)
 
 if __name__ == '__main__':
     unittest.main()
