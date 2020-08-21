9c4fa82ab3e375057b0de3447864d13e2cb6adc6
Evening <johnchangqi@hotmail.com>
2020-08-21 21:33:10 +0800

    Implement slide and tohsv funcs

--- src/frmodel/base/D2/frame2D.py
+++ src/frmodel/base/D2/frame2D.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from typing import List
+from typing import List, Tuple
 
 import numpy as np
+from skimage.color import rgb2hsv
 from PIL import Image
 
 from frmodel.base.D2.channel2D import Channel2D
@@ -18,14 +20,9 @@ D_TYPE: np.dtype = \
 class Frame2D:
     """ A Frame is an alias to an Frame, however, it holds more than just the XYRGB Channels.
 
-    Note that due to the nature of np arrays, it cannot be any irregular shape.
-    The structure is very simple, it's a 3D object always. That is, the X, Y, ?.
+    The underlying representation is a 2D array, each cells holds a structured array, consisting of RGB Channels.
 
-    Because X, Y are directional, we can place it logically it in an array index.
-    However, RGB(and other spectral ranges) are not directional, and are dependent on the XYZ.
-
-    Hence they are placed in a structured array.
-
+    It's easily extendable for more channels, but for now we only have the RGB.
     """
 
     data: np.ndarray
@@ -62,6 +59,8 @@ class Frame2D:
               method: SplitMethod = SplitMethod.DROP) -> List[Frame2D]:
         """ Splits the current Frame into windows of specified size.
 
+        This operation is much faster but cannot have overlapping windows
+
         E.g.::
 
             frame.split(
@@ -88,6 +87,45 @@ class Frame2D:
             # We will use a conditional to drop any images that is "cropped"
             return [Frame2D(s) for s in spl if s.shape[axis] == by]
 
+    def slide_xy(self, by, stride=1):
+        """ Short hand for sliding by both axes.
+
+        Slides by X first, then Y.
+
+        E.g.::
+
+            | 1 | 6  | 11 | 16 |
+            | 2 | 7  | 12 | 17 |
+            | 3 | 8  | 13 | 18 |
+            | 4 | 9  | 14 | 19 |
+            | 5 | 10 | 15 | 20 |
+
+            [[1,2,3,4,5],[6,7,8,...], ...]
+
+        """
+        return [f.slide(by=by, stride=stride, axis=CONSTS.AXIS.Y)
+                for f in self.slide(by=by, stride=stride, axis=CONSTS.AXIS.X)]
+
+    def slide(self, by, stride, axis=CONSTS.AXIS.X):
+        """ Slides a window along an axis and grabs that window as a new Frame2D
+
+        This operation is slower due to looping but allows for overlapping windows
+
+        If the window meets the edge, it will be dropped.
+
+        :param by: The size of the window to slide
+        :param stride: The stride of the window on specified axis
+        :param axis: Axis to slide on
+        :return:
+        """
+
+        if axis == CONSTS.AXIS.X:
+            return [Frame2D(self.data[:, i: i + by])
+                    for i in range(0, self.width() - by + 1, stride)]
+        elif axis == CONSTS.AXIS.Y:
+            return [Frame2D(self.data[i: i + by, :])
+                    for i in range(0, self.height() - by + 1, stride)]
+
     @staticmethod
     def from_image(file_path: str) -> Frame2D:
         """ Creates an instance using the file path. """
@@ -95,7 +133,23 @@ class Frame2D:
         ar = np.asarray(img).view(dtype=D_TYPE).squeeze()
         return Frame2D(ar)
 
-    def data_flatten(self):
+    def to_hsv(self) -> np.ndarray:
+        """ Converts to a HSV Array """
+        return rgb2hsv(self.data_unstruct())
+
+    def to_ex_g(self, modified=False) -> np.ndarray:
+        """ Calculates the excessive green index
+
+        Original: 2g - 1r - 1b
+        Modified: 1.262G - 0.884r - 0.331b
+        """
+
+    def to_ex_gr(self) -> np.ndarray:
+        """ Calculates the excessive green minus excess red index
+
+        """
+
+    def data_unstruct(self) -> np.ndarray:
         """ Returns the data as a regular numpy array """
         shape = (*self.shape[0:2], -1)
         return self.data.view(dtype=np.uint8).reshape(shape)
@@ -110,12 +164,12 @@ class Frame2D:
                 .reshape([*self.shape[0:2], -1]))\
             .save(file_path, **kwargs)
 
-    def size(self):
+    def size(self) -> np.ndarray:
         """ Returns the number of pixels """
         return self.data.size
 
     @property
-    def shape(self):
+    def shape(self) -> Tuple:
         return self.data.shape
 
     def height(self):
