be60a5fdd123615611820406bb70578bf5ccc61c
Evening <johnchangqi@hotmail.com>
2020-09-01 17:57:20 +0800

    Change CHANNEL to use numerical index

--- src/frmodel/base/consts.py
+++ src/frmodel/base/consts.py
@@ -4,11 +4,21 @@ class CONSTS:
     This is to facilitate magic constants/numbers around the program.
     """
     class CHANNEL:
-        RED = "R"
-        GREEN = "G"
-        BLUE = "B"
+        RED = 0
+        GREEN = 1
+        BLUE = 2
 
     class AXIS:
         X = 1  # Not sure why is it like this, may need to "fix" this.
         Y = 0  # Images are stored as height x width, e.g. 1080 x 1920.
         Z = 2
+
+
+""" Deprecated D_TYPE on branch destruct
+
+D_TYPE: np.dtype = \
+        np.dtype([(CONSTS.CHANNEL.RED,   'u1'),   # 0 - 255
+                  (CONSTS.CHANNEL.GREEN, 'u1'),   # 0 - 255
+                  (CONSTS.CHANNEL.BLUE,  'u1')])  # 0 - 255
+
+"""
