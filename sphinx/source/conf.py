edb70064cee5f91a7b2618c1046fadd8751102f1
Evening <johnchangqi@hotmail.com>
2020-09-02 00:13:38 +0800

    Fix doc versioning

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Eve-ning'
 author = 'Eve-ning'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
