6d97820c9daa0122602ded51515907a207d1b37c
Evening <johnchangqi@hotmail.com>
2020-09-04 17:25:25 +0800

    Fix RGBXY ordering problem

--- src/frmodel/base/D2/frame2D.py
+++ src/frmodel/base/D2/frame2D.py
@@ -137,21 +137,24 @@ class Frame2D:
         return Frame2D(ar)
 
     @staticmethod
+    def from_rgbxy_(ar: np.ndarray, xy_pos=(3,4), width=None, height=None) -> Frame2D:
+        """ Rebuilds the frame with XY values. XY should be of integer values, otherwise, will be casted.
 
         Note that RGB channels MUST be on index 0, 1, 2 else some functions may break.
 
+        The frame will be rebuild and all data will be retained, including XY.
 
         :param ar: The array to rebuild
         :param xy_pos: The positions of X and Y.
         """
-        max_x = np.max(ar[:,xy_pos[0]])
-        max_y = np.max(ar[:,xy_pos[1]])
+        max_y = height if height else np.max(ar[:,xy_pos[1]]) + 1
+        max_x = width if width else np.max(ar[:,xy_pos[0]]) + 1
 
-        fill = np.zeros((ceil(max_x) + 1, ceil(max_y) + 1, ar.shape[-1]), dtype=ar.dtype)
+        fill = np.zeros(( ceil(max_y), ceil(max_x), ar.shape[-1]), dtype=ar.dtype)
 
         # Vectorized X, Y <- RGBXY... Assignment
-        fill[ar[:, xy_pos[0]].astype(int),
-             ar[:, xy_pos[1]].astype(int)] = ar[:]
+        fill[ar[:, xy_pos[1]].astype(int),
+             ar[:, xy_pos[0]].astype(int)] = ar[:]
 
         return Frame2D(fill)
 
