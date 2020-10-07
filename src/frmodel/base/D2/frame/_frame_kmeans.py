e7fddc3112d0d15a309c29c0d93e56cff461e2b8
Evening <johnchangqi@hotmail.com>
2020-10-07 22:07:48 +0800

    Optimize kmeans calling
    By fragmenting code and moving it to a separate class, we can achieve a smaller code call

--- src/frmodel/base/D2/frame/_frame_kmeans.py
+++ src/frmodel/base/D2/frame/_frame_kmeans.py
@@ -13,53 +13,11 @@ class _Frame2DKmeans(ABC):
     @abstractmethod
     def data_flatten(self): ...
 
-    def kmeans(self,
-               clusters,
-               fit_indexes,
-               sample_weight=None,
-               verbose=False,
-               scaler=sk_normalize,
-               plot_figure=False,
-               xy_indexes=(3 ,4),
-               scatter_size=0.2
-               ) -> KMeans2D:
-        """ Fits a KMeans on current frame, on certain axes
+    def kmeans(self, model: KMeans) -> KMeans2D:
+        """ Creates a KMeans Object from current data
 
-        Example::
-
-            frame_idxs = frame.get_idxs(xy=True,hsv=True,veg=True)
-            frame_idxs.kmeans(clusters=5, fit_indexes=[2,3,4,5],
-                              plot_figure=True,xy_indexes=(0,1),scatter_size=1)
-
-        This will firstly get the coordinate, hsv and veg indexes, placing it in a df
-
-        :param clusters: The number of clusters
-        :param fit_indexes: The indexes to .fit()
-        :param sample_weight: The sample weight for each record, if any. Can be None.
-        :param verbose: Whether to print out the KMeans' verbose log
-        :param scaler: The scaler to use, must be a callable(np.ndarray)
-        :param plot_figure: Whether to plot a figure or not, using plt.gcf()
-        :param xy_indexes: The indexes of XY. Must be present for plotting.
-        :param scatter_size: Size of the marker on the scatter plot
-        :return:
+        :param model: KMeans Model
+        :returns: KMeans2D Instance
         """
-        flat = self.data_flatten()
-        frame_xy_trans = scaler(flat[:, fit_indexes])
-        km = KMeans(n_clusters=clusters, verbose=verbose) \
-            .fit(frame_xy_trans,
-                 sample_weight=frame_xy_trans[:, sample_weight] if np.all(sample_weight) else None)
-        if plot_figure:
-            df = pd.DataFrame(np.append(flat, km.labels_[... ,np.newaxis] ,axis=-1))
-            df.columns = [f"c{e}" for e, _ in enumerate(df.columns)]
-            fg = sns.lmplot(data=df,
-                            x=f'c{xy_indexes[0]}',
-                            y=f'c{xy_indexes[1]}',
-                            hue=df.columns[-1],
-                            fit_reg=False,
-                            legend=True,
-                            legend_out=True,
-                            scatter_kws={"s": scatter_size})
-            fg.ax.set_aspect('equal')
-            fg.ax.invert_yaxis()
-            return KMeans2D(km, fg)
-        return KMeans2D(km)
+        return KMeans2D(model, self.data_flatten())
+
--- src/frmodel/base/D2/kmeans2D.py
+++ src/frmodel/base/D2/kmeans2D.py
@@ -1,15 +1,63 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
-import matplotlib.pyplot as plt
 from seaborn import FacetGrid
 
 from sklearn.cluster import KMeans
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import normalize
 
 
 @dataclass
 class KMeans2D:
 
     model: KMeans
-    plot: FacetGrid = None
+    data: np.ndarray
 
+    def fit(self,
+            fit_indexes,
+            sample_weight=None,
+            scaler=normalize) -> KMeans:
+        """ Fits a KMeans on current frame, on certain axes
+
+        Example::
+
+            frame_idxs = frame.get_idxs(xy=True,hsv=True,veg=True)
+            frame_idxs.kmeans(clusters=5, fit_indexes=[2,3,4,5],
+                              plot_figure=True,xy_indexes=(0,1),scatter_size=1)
+
+        This will firstly get the coordinate, hsv and veg indexes, placing it in a df
+
+        :param fit_indexes: The indexes to .fit()
+        :param sample_weight: The sample weight for each record, if any. Can be None.
+        :param scaler: The scaler to use, must be a callable(np.ndarray)
+        :return:
+        """
+        frame_xy_trans = scaler(self.data[:, fit_indexes])
+        km_est = self.model.fit(frame_xy_trans,
+                                sample_weight=frame_xy_trans[:, sample_weight] if np.all(sample_weight) else None)
+        return km_est
+
+    def plot(self,
+             km_est: KMeans,
+             xy_indexes=(3 ,4),
+             scatter_size=0.2) -> FacetGrid:
+        df = pd.DataFrame(np.append(self.data, km_est.labels_[..., np.newaxis], axis=-1))
+        df.columns = [f"c{e}" for e, _ in enumerate(df.columns)]
+
+        fg = sns.lmplot(data=df,
+                        x=f'c{xy_indexes[0]}',
+                        y=f'c{xy_indexes[1]}',
+                        hue=df.columns[-1],
+                        fit_reg=False,
+                        legend=True,
+                        legend_out=True,
+                        scatter_kws={"s": scatter_size})
+        fg.ax.set_aspect('equal')
+        fg.ax.invert_yaxis()
+
+        return fg
 
 
