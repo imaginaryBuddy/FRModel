ce237b2786dd1527606f38b44d6618a05aec24c5
Evening <johnchangqi@hotmail.com>
2020-10-11 17:13:35 +0800

    Add Scoring method

--- src/frmodel/base/D2/kmeans2D.py
+++ src/frmodel/base/D2/kmeans2D.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+
+from PIL import Image
+from scipy.stats import rankdata
 from seaborn import FacetGrid
 
 from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.preprocessing import normalize
 
 
 @dataclass
@@ -46,8 +48,54 @@ class KMeans2D:
 
     def score(self,
               score_file_path: str):
-        """ Scores the current Kmeans fit
+        """ Scores the current Kmeans model with a scoring image
 
-        :rtype: object
+        :param score_file_path: The file path to the scoring image.
+        :return: Count Array, Score out of 1.
         """
+
+        # Convert to array
+        im: Image.Image = Image.open(score_file_path).convert('LA')
+        ar = np.asarray(im)[..., 0]
+
+        # Convert Image Grayscale (0-255) to quantized rank
+        # This will only work if all Grayscale values are unique.
+        ar = rankdata(ar, method='dense') - 1
+
+        # Count each unique pair occurrence and return count.
+        # Because return_count returns separately, we vstack it
+        # Then we transpose the data for iterrows() op
+        ar_0 = \
+            np.vstack(
+                np.unique(axis=1, return_counts=True,
+                          ar=np.vstack([self.model.labels_, ar]))).transpose()
+
+        # This sorts by the last column (Counts)
+        ar_0: np.ndarray = ar_0[ar_0[:, -1].argsort()[::-1]]
+
+        # There's no simple way to get the maximum unique of 2 dimensions I believe
+        # We'll loop through the cells using a naive approach
+        # This approach is naive because if we were to permutate all possible
+        # combinations, we'll end up with a really large list.
+        # This is not ideal if we want to scale this up for more trees
+        # However, it's not a hard limitation.
+
+        # We have the following array structure
+        # PREDICT ACTUAL COUNT
+        # The catch is that predict and actual cannot appear more than once.
+
+        visited_pred = []
+        visited_act = []
+        counts = []
+        for r in ar_0:
+            if r[0] in visited_pred or r[1] in visited_act:
+                continue
+            else:
+                visited_pred.append(int(r[0]))
+                visited_act.append(int(r[1]))
+                counts.append(r)
+
+        ar_1 = np.asarray(counts)
+        return ar_1, np.sum(ar_1[:, -1]) / (im.size[0] * im.size[1])
+
 
