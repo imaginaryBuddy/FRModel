8387a684e423f754e5151da5abb9b1ad6214b5a0
Evening <johnchangqi@hotmail.com>
2020-10-11 18:45:14 +0800

    Add Score test

--- tests/base/D2/test_d2.py
+++ tests/base/D2/test_d2.py
@@ -12,6 +12,7 @@ class TestD2(unittest.TestCase):
         cls.frame = Frame2D.from_image(f"{_RSC}/imgs/chestnut_0/frame10000ms.jpg")
         cls.frame_window = cls.frame.split_xy(100)[0][0]
         cls.window = 100
+        cls._RSC = _RSC
         cls.channels = 3
 
 if __name__ == '__main__':
