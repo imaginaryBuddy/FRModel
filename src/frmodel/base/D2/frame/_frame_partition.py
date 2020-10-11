5af9d79cabdd85e9b8f1ebb42a6c2fb89c2f8fc7
Evening <johnchangqi@hotmail.com>
2020-10-11 17:55:24 +0800

    Clarify Partition cutting method/axis

--- src/frmodel/base/D2/frame/_frame_partition.py
+++ src/frmodel/base/D2/frame/_frame_partition.py
@@ -31,7 +31,7 @@ class _Frame2DPartition(ABC):
 
     def split_xy(self,
                  by: int,
-                 method: SplitMethod = SplitMethod.DROP):
+                 method: SplitMethod = SplitMethod.DROP) -> List[List[_Frame2DPartition]]:
         """ Short hand for splitting by both axes.
 
         Splits by X first, then Y.
@@ -46,48 +46,48 @@ class _Frame2DPartition(ABC):
 
             [[1,2,3,4,5],[6,7,8,...], ...]
 
+
+        :param by: Size of each piece
+        :param method: Method of splitting
+        :return: List of List of Frame2D
         """
-        return [f.split(by, axis=CONSTS.AXIS.X, method=method)
-                for f in self.split(by, axis=CONSTS.AXIS.Y, method=method)]
+        return [f.split(by, axis_cut=CONSTS.AXIS.X, method=method)
+                for f in self.split(by, axis_cut=CONSTS.AXIS.Y, method=method)]
 
     def split(self,
               by: int,
-              axis: CONSTS.AXIS = CONSTS.AXIS.Y,
+              axis_cut: CONSTS.AXIS = CONSTS.AXIS.Y,
               method: SplitMethod = SplitMethod.DROP) -> List[_Frame2DPartition]:
         """ Splits the current Frame into windows of specified size.
 
         This operation is much faster but cannot have overlapping windows
 
-        E.g.::
+        :param by: Size of each piece
+        :param axis_cut: Axis to cut
+        :param method: Method of splitting
+        :return: List of Frame2D
 
-            frame.split(
-                by = 50,
-                axis = CONSTS.AXIS.Y,
-                method = Frame2D.SplitMethod.DROP
-            )
-
-        Will slice the images vertically, drops the last slice if it's not perfectly divisible.
         """
 
         # Pre-process by as modified by_
         # np.split_array splits it by the number of slices generated,
         # we need to transform this into the slice locations
-        if axis == CONSTS.AXIS.Y:
+        if axis_cut == CONSTS.AXIS.X:
             by_ = np.arange(by, self.width(), by)
-        elif axis == CONSTS.AXIS.X:
+        elif axis_cut == CONSTS.AXIS.Y:
             by_ = np.arange(by, self.height(), by)
         else:
-            raise TypeError(f"Axis {axis} is not recognised. Use CONSTS.AXIS class.")
+            raise TypeError(f"Axis {axis_cut} is not recognised. Use CONSTS.AXIS class.")
 
-        spl = np.array_split(self.data, by_, axis=axis)
+        spl = np.array_split(self.data, by_, axis=axis_cut)
         if method == _Frame2DPartition.SplitMethod.CROP:
             # By default, it'll just "crop" the edges
             return [self.init(s) for s in spl]
         elif method == _Frame2DPartition.SplitMethod.DROP:
             # We will use a conditional to drop any images that is "cropped"
-            return [self.init(s) for s in spl if s.shape[axis] == by]
+            return [self.init(s) for s in spl if s.shape[axis_cut] == by]
 
-    def slide_xy(self, by, stride=1):
+    def slide_xy(self, by, stride=1) -> List[List[_Frame2DPartition]]:
         """ Short hand for sliding by both axes.
 
         Slides by X first, then Y.
@@ -102,25 +102,31 @@ class _Frame2DPartition(ABC):
 
             [[1,2,3,4,5],[6,7,8,...], ...]
 
-        """
-        return [f.slide(by=by, stride=stride, axis=CONSTS.AXIS.X)
-                for f in self.slide(by=by, stride=stride, axis=CONSTS.AXIS.Y)]
 
-    def slide(self, by, stride, axis=CONSTS.AXIS.Y):
+        :param by: Size of each piece
+        :param stride: Stride when sliding 
+        :return: List of List of Frame2D
+
+        """
+        return [f.slide(by=by, stride=stride, axis_cut=CONSTS.AXIS.X)
+                for f in self.slide(by=by, stride=stride, axis_cut=CONSTS.AXIS.Y)]
+
+    def slide(self, by, stride, axis_cut: CONSTS.AXIS = CONSTS.AXIS.Y) -> List[_Frame2DPartition]:
         """ Slides a window along an axis and grabs that window as a new Frame2D
 
         This operation is slower due to looping but allows for overlapping windows
 
         If the window meets the edge, it will be dropped.
 
-        :param by: The size of the window to slide
-        :param stride: The stride of the window on specified axis
-        :param axis: Axis to slide on
+        :param by: Size of each piece
+        :param stride: Stride when sliding
+        :param axis_cut: Axis to cut
+        :return: List of Frame2D
         """
 
-        if axis == CONSTS.AXIS.Y:
+        if axis_cut == CONSTS.AXIS.X:
             return [self.init(self.data[:, i: i + by])
                     for i in range(0, self.width() - by + 1, stride)]
-        elif axis == CONSTS.AXIS.X:
+        elif axis_cut == CONSTS.AXIS.Y:
             return [self.init(self.data[i: i + by, :])
                     for i in range(0, self.height() - by + 1, stride)]
