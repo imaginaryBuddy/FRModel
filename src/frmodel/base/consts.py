200cf143a9f6514d3d79b3771cd3e2e397da0d2b
Evening <johnchangqi@hotmail.com>
2020-12-11 15:08:04 +0800

    Update constants to labels to uniquely identify ixs

--- src/frmodel/base/consts.py
+++ src/frmodel/base/consts.py
@@ -4,9 +4,33 @@ class CONSTS:
     This is to facilitate magic constants/numbers around the program.
     """
     class CHANNEL:
-        RED = 0
-        GREEN = 1
-        BLUE = 2
+        RED         = "R"
+        GREEN       = "G"
+        BLUE        = "B"
+        HUE         = "H"
+        SATURATION  = "S"
+        VALUE       = "V"
+        NDI         = "NDI"
+        EX_G        = "EX_G"
+        MEX_G       = "MEX_G"
+        EX_GR       = "EX_GR"
+        VEG         = "VEG"
+        NDVI        = "NDVI"
+        GNDVI       = "GNDVI"
+        OSAVI       = "OSAVI"
+        NDRE        = "NDRE"
+        LCI         = "LCI"
+
+        class GLCM:
+            CON_RED   = "CON_R"
+            CON_GREEN = "CON_G"
+            CON_BLUE  = "CON_B"
+            COR_RED   = "COR_R"
+            COR_GREEN = "COR_G"
+            COR_BLUE  = "COR_B"
+            ENT_RED   = "ENT_R"
+            ENT_GREEN = "ENT_G"
+            ENT_BLUE  = "ENT_B"
 
     class AXIS:
         X = 0
