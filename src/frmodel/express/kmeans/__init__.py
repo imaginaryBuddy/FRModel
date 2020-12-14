4c9c337318b5f29962ae87d2d4d22be8d796d164
Evening <johnchangqi@hotmail.com>
2020-12-14 18:36:42 +0800

    Update init to deprecate old scoring and use new

--- src/frmodel/express/kmeans/__init__.py
+++ src/frmodel/express/kmeans/__init__.py
@@ -1,5 +1,3 @@
-from frmodel.express.kmeans.kmeans import kmeans, kmeans_score, kmeans_matrix, \
-    kmeans_matrix_2dscore_heatmap, kmeans_matrix_3dscore_heatmap
+from frmodel.express.kmeans.kmeans_scoring import kmeans_scoring_12122020
 
-__all__ = ['kmeans', 'kmeans_matrix', 'kmeans_score', 'kmeans_matrix_3dscore_heatmap',
-           'kmeans_matrix_2dscore_heatmap']
+__all__ = ['kmeans_scoring_12122020']
