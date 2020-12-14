dd377f3ba31f3c004f233fc538aec796811bcb18
Evening <johnchangqi@hotmail.com>
2020-12-14 15:41:40 +0800

    Add convolution method into Frame. Fix cropping issue

--- src/frmodel/base/D2/frame/_frame_image.py
+++ src/frmodel/base/D2/frame/_frame_image.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from PIL import Image
+from scipy.signal import fftconvolve
+from scipy.signal.windows import gaussian
 from skimage.transform import rescale
 
 if TYPE_CHECKING:
@@ -22,15 +24,36 @@ class _Frame2DImage(ABC):
              bottom:int = 0,
              left:int = 0) -> _Frame2DImage:
         """ Crops the frame by specifying how many rows/columns to remove from each side."""
+        self: 'Frame2D'
+        return self.create(self.data[top:-bottom or None, left:-right or None, ...],
+                           self.labels)
 
     def crop_glcm(self, glcm_radius):
         """ Crops the frame to match GLCM cropping. """
-        return self.crop(glcm_radius + 1, glcm_radius, glcm_radius + 1, glcm_radius)
+        return self.crop(glcm_radius + 1, glcm_radius + 1, glcm_radius, glcm_radius)
 
     def save(self, file_path: str, **kwargs) -> None:
         """ Saves the current Frame file """
         self: 'Frame2D'
         Image.fromarray(self.data_rgb().astype(np.uint8)).save(file_path, **kwargs)
+
+    def convolute(self, radius: int, method: str = 'nearest') -> Frame2D:
+        """ Convolutes the Frame. """
+
+        # We trim the frame so that the new glcm can fit
+        # We also average the shifted frame with the current
+        kernel_diam = radius * 2 + 1
+        if method == 'nearest':
+            kernel = np.zeros([kernel_diam + 1, kernel_diam + 1, 1])
+            kernel[kernel.shape[0] // 2,
+                   kernel.shape[1] // 2] = 1
+        else:  # 'average'
+            kernel = np.outer(gaussian(kernel_diam + 1, radius),
+                              gaussian(kernel_diam + 1, radius))
+            kernel = np.expand_dims(kernel, axis=-1)
+
+        self: 'Frame2D'
+        return self.create(fftconvolve(self.data, kernel, mode='valid', axes=[0, 1]), self.labels)
 
     def _rescale(self,
                 scale: float,
