6c554b08066729b018e845dc79bde9ab06784685
Evening <johnchangqi@hotmail.com>
2021-03-05 12:15:49 +0800

    Update pathing for rsc

--- tests/base/D2/test_d2.py
+++ tests/base/D2/test_d2.py
@@ -9,7 +9,7 @@ class TestD2(unittest.TestCase):
 
     @classmethod
     def setUp(cls) -> None:
-        cls.frame = Frame2D.from_image(f"{_RSC}/imgs/chestnut_0/frame10000ms.jpg")
+        cls.frame = Frame2D.from_image(f"{_RSC}/imgs/rgb/chestnut/frame10000ms.jpg")
         cls.frame_window = cls.frame.split_xy(100)[0][0]
         cls.window = 100
         cls._RSC = _RSC
