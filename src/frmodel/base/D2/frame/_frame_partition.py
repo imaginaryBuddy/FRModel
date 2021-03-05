28f41c3f8f18125fb7c62bdce2390e8bb00e2e06
Evening <johnchangqi@hotmail.com>
2021-03-05 20:51:12 +0800

    Split views to separate calls for Frame and np return

--- src/frmodel/base/D2/frame/_frame_partition.py
+++ src/frmodel/base/D2/frame/_frame_partition.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import List, TYPE_CHECKING, Union
+from typing import List, TYPE_CHECKING
 
 import numpy as np
 from skimage.util import view_as_windows, view_as_blocks
@@ -15,65 +15,90 @@ from warnings import warn
 
 
 class _Frame2DPartition(ABC):
-    class SplitMethod:
-        DROP = 0
-        CROP = 1
-
-        # Require to support padding? Not implemented yet.
 
     def view_windows(self: 'Frame2D',
                      height: int,
                      width: int,
                      height_step: int,
-                     width_step: int,
-                     as_frames: bool = True) -> Union[List[List['Frame2D']], np.ndarray]:
+                     width_step: int) -> np.ndarray:
         """ Partitions the image into windows using striding
 
-        Note that the parameters must cleanly divide the image.
+        Note that the parameters must cleanly divide the image else it'll be cropped
         
         E.g. 200 x 200 cannot be 100 Step 3 x 100 Step 7
 
-        If an np.ndarray is expected, the dims are
-            [RowWindow, ColWindow, Row, Col, Channels]
+        The dims are [RowBlock, ColBlock, Row, Col, Channels]
 
         :param height: Height of expected window
         :param width: Width of expected window
         :param height_step: Step of height window
-        :param width: Steo of width window
-        :param as_frames: Whether to output as a list of Frame2D or a 5D np.ndarray
+        :param width_step: Step of width window
         """
 
-        view: np.ndarray = view_as_windows(self.data, (height, width, self.shape[-1]),
-                                           (height_step, width_step, 1)).squeeze(2)
+        view: np.ndarray =\
+            view_as_windows(self.data,
+                            (height, width, self.shape[-1]),
+                            (height_step, width_step, 1)).squeeze(2)
         # [HW, WW, H, W, C]
-        if as_frames:
-            return [[Frame2D(view[h, w, 0], self.labels)
-                     for h in range(view.shape[0])] for w in range(view.shape[1])]
-        else:
-            return view
+        return view
+
+    def view_windows_as_frames(self: 'Frame2D',
+                               height: int,
+                               width: int,
+                               height_step: int,
+                               width_step: int) -> List[List['Frame2D']]:
+        """ Partitions the image into windows using striding
+
+        Note that the parameters must cleanly divide the image else it'll be cropped
+
+        E.g. 200 x 200 cannot be 100 Step 3 x 100 Step 7
+
+        :param height: Height of expected window
+        :param width: Width of expected window
+        :param height_step: Step of height window
+        :param width_step: Step of width window
+        """
+        view = self.view_windows(height, width, height_step, width_step)
+        return [[self.create(view[h, w], self.labels)
+                 for h in range(view.shape[0])] for w in range(view.shape[1])]
 
     def view_blocks(self: 'Frame2D',
                     height: int,
-                    width: int,
-                    as_frames: bool = True) -> Union[List[List['Frame2D']], np.ndarray]:
+                    width: int) -> np.ndarray:
         """ Partitions the image into blocks using striding
 
         Note that the height and width must be a divisor.
 
-        If an np.ndarray is expected, the dims are
-            [RowBlock, ColBlock, Row, Col, Channels]
+        The dims are [RowBlock, ColBlock, Row, Col, Channels]
 
         :param height: Height of expected block, must be int divisor of original height
         :param width: Width of expected block, must be int divisor of original width
-        :param as_frames: Whether to output as a list of Frame2D or a 5D np.ndarray
         """
-        view: np.ndarray = view_as_blocks(self.data, (height, width, self.shape[-1])).squeeze(2)
+        view: np.ndarray =\
+            view_as_blocks(self.data,
+                           (height, width, self.shape[-1])).squeeze(2)
         # [HW, WW, H, W, C]
-        if as_frames:
-            return [[Frame2D(view[h, w, 0], self.labels)
-                     for h in range(view.shape[0])] for w in range(view.shape[1])]
-        else:
-            return view
+        return view
+
+    def view_blocks_as_frames(self: 'Frame2D',
+                              height: int,
+                              width: int) -> List[List['Frame2D']]:
+        """ Partitions the image into blocks using striding
+
+        Note that the height and width must be a divisor.
+
+        :param height: Height of expected block, must be int divisor of original height
+        :param width: Width of expected block, must be int divisor of original width
+        """
+        view = self.view_blocks(height, width)
+
+        return [[self.create(view[h, w], self.labels)
+                 for h in range(view.shape[0])] for w in range(view.shape[1])]
+
+    # -------- DEPRECATED FUNCTIONS --------
+    class SplitMethod:
+        DROP = 0
+        CROP = 1
 
     def split(self: 'Frame2D',
               by: int,
--- tests/base/D2/partition/test_partition.py
+++ tests/base/D2/partition/test_partition.py
@@ -7,13 +7,34 @@ from tests.base.D2Fixture.test_fixture import TestD2Fixture
 
 class TestFrame(TestD2Fixture):
 
+    def test_view_windows(self):
+        height = self.frame.height()
+        width = self.frame.width()
+        step = 100
+        windows = self.frame.view_windows(height // 2, width // 2, step, step)
+        self.assertEqual(((height - (height // 2)) // step + 1,  # +1: Fencepost error
+                          (width - (width // 2)) // step + 1,    # +1: Fencepost error
+                           height // 2, width // 2, self.frame.shape[-1]),
+                         windows.shape)
+
+    def test_view_blocks(self):
+        height = self.frame.height()
+        width = self.frame.width()
+        blocks = 5
+        windows = self.frame.view_blocks(height // blocks, width // blocks)
+        self.assertEqual((blocks, blocks,
+                          height // blocks, width // blocks, self.frame.shape[-1]),
+                         windows.shape)
+
     def test_split_xy(self):
+        """ Deprecated Function """
         # Split by both X and Y
         frames = self.frame.split_xy(by=self.window, method=Frame2D.SplitMethod.DROP)
 
         self.assertEqual((self.window, self.window, self.channels), frames[0][0].shape)
 
     def test_split(self):
+        """ Deprecated Function """
         # Split by X Axis only, horizontal slices
         frames = self.frame.split(by=self.window, method=Frame2D.SplitMethod.DROP, axis_cut=CONSTS.AXIS.X)
 
@@ -21,12 +42,14 @@ class TestFrame(TestD2Fixture):
         self.assertEqual(self.window, frames[0].shape[0])
 
     def test_slide_xy(self):
+        """ Deprecated Function """
         # Slide by both X and Y
         frames = self.frame.slide_xy(by=self.window, stride=self.window // 2)
 
         self.assertEqual((self.window, self.window, self.channels), frames[0][0].shape)
 
     def test_slide(self):
+        """ Deprecated Function """
         # Slide by X Axis only, horizontal slices
         frames = self.frame.slide(by=self.window, stride=self.window // 2, axis_cut=CONSTS.AXIS.X)
 
